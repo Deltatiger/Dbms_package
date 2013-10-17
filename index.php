78d5bfc14b22232ee9713ef31b65f4e71f8087fb
Deltatiger <deltatiger94@gmail.com>
2013-10-17 22:02:44 +0530

    Designing Template - 2
    Some more fixups

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
     //This is the main file.
     include 'includes/config.php';
     
+    $sql = "SELECT `c_name`, `c_id` FROM `{$db->name()}`.`dbms_category`";
+    $query = $db->query($sql);
+    $catOptions = '<ul id="catOptions">';
+    while($row = $db->result($query))   {
+        $catOptions .= '<li> <a href="#" id="catName" data-catId="'.$row->c_id.'">'.$row->c_name.'</a></li>';
+    }
+    $catOptions .= '<ul>';
+    $template->setTemplateVar('catagories', $catOptions);
+    
     $template->setPage('index');
     $template->setPageTitle('myKart - Home Page');
     
--- templates/t_index.php
+++ templates/t_index.php
@@ -1,4 +1,12 @@
 <!-- This is the main Index page that will hold everything -->
 <div id ="mBody">
-    This is the main body of the whole Site.
+    <!-- This page is divided into 2 parts -->
+    <div id="mIndexLeft">
+        <?php
+            $this->printVar('category');
+        ?>
+    </div>
+    <div id="mIndexRight">
+        Test
+    </div>
 </div>
